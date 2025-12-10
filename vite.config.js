6b305e3e8589c690fbf223e08cd1a3927dfea786
Amanda <amandyanethranja@gmail.con>
2025-12-10 23:50:34 +0530

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/KLANK-portfolio-my/', // Your repo name here
+  base: '/KLANK-portfolio-my/',   // ← THIS LINE IS VERY IMPORTANT
 })
